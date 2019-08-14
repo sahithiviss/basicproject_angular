ecd0f741f63cd2376ed2b4a233326626eec36c43
sahithiviss <sahithivsl@gmail.com>
2019-08-14 11:24:43 +0530

    angular first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basicproject';
+  title = 'Courses';
+  courses = [
+    {
+      name: "Java",
+      id: 1,
+      price: 8000
+    },
+    {
+      name: "spring",
+      id: 2, price: 6000
+    },
+    {
+      name: "Hibernate",
+      id: 3,
+      price: 2000
+    },
+    {
+      name: "angular",
+      id: 4,
+      price: 3000
+    },
+    {
+      name: "JPA",
+      id: 5,
+      price: 9000
+    },
+    {
+      name: "Maven",
+      id: 6,
+      price: 1000
+    }
+  ]
 }
