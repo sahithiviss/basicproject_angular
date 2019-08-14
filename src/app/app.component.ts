6bdff6b91626b69083113920d1b697862fb97480
sahithiviss <sahithivsl@gmail.com>
2019-08-14 11:58:56 +0530

    added card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { headersToString } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-root',
@@ -11,31 +12,40 @@ export class AppComponent {
     {
       name: "Java",
       id: 1,
-      price: 8000
+      price: 8000,
+      duration:4,
+      offer:true
     },
     {
       name: "spring",
-      id: 2, price: 6000
+      id: 2, 
+      price: 6000
     },
     {
       name: "Hibernate",
       id: 3,
-      price: 2000
+      price: 2000,
+      duration:4,
+      offer:200
     },
     {
       name: "angular",
       id: 4,
+      duration:4,
       price: 3000
     },
     {
       name: "JPA",
       id: 5,
+      duration:4,
       price: 9000
     },
     {
       name: "Maven",
       id: 6,
-      price: 1000
+      duration:4,
+      price: 1000,
+      offer:true
     }
   ]
 }
